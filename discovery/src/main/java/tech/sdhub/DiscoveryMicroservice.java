094d2ba4b0c6eacfe15fcdb2f6659b3deb36c45c
ilya-noize <shumakoff.ilya@yandex.ru>
2024-09-13 20:47:17 +0300

    Microserv: Users, Company; pom.xml, psvm, application.yaml, schema.sql for everyone module;

---
+++ discovery/src/main/java/tech/sdhub/DiscoveryMicroservice.java
@@ -0,0 +1,14 @@
+package tech.sdhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class DiscoveryMicroservice {
+    public static void main(String[] args) {
+
+        SpringApplication.run(DiscoveryMicroservice.class, args);
+    }
+}
